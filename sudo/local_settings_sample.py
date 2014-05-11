2a1a57339f423bf1837725b284db107f735072c6
S.R.Manikandasriram <srmanikandasriram@gmail.com>
2014-05-11 23:42:06 +0530

    Minor changes
    added local_settings_sample

---
+++ sudo/local_settings_sample.py
@@ -0,0 +1,26 @@
+# Django settings for sudo project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sudo',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+SITE_ID = 1
+
+SITE_URL = 'http://127.0.0.1:8000/'
